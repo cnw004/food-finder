ed4f43b576ae95de4bcdf47070fba5865e218e18
Cole Whitley <cwhitley@deloitte.com>
2019-04-24 13:11:06 -0400

    main layout configured, time to work on functionality'

--- fp/cli.py
+++ fp/cli.py
@@ -3,13 +3,14 @@ import click
 
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def main(count, name):
+# @click.argument('action') will implement later
+@click.option('--count', default=5, help='Number of options you want displayed.')
+@click.option('--type', default='any',
+              help='The type of food you are looking for.')
+def main(count, type):
     """Simple program that greets NAME for a total of COUNT times."""
     for _ in range(count):
-        click.echo('Hello %s!' % name)
+        click.echo('Searching for %s food!' % type)
 
 if __name__ == "__main__":
     main()
