6342547019e1abd3f58acd18c8bfcd5994cdddb2
Cole Whitley <cwhitley@deloitte.com>
2019-04-25 10:11:38 -0400

    added option for price, json, and sort type'

--- fp/cli.py
+++ fp/cli.py
@@ -10,11 +10,14 @@ def main():
     pass
 
 @main.command()
-@click.option('--kind', '-k', type=str, multiple=True,help='Kind of business you are searching for (i.e. chinese, bbq, etc.)')
+@click.option('--kind', '-k', type=str, multiple=True, help='Kind of business you are searching for (i.e. chinese, bbq, etc.)')
 @click.option('--number', '-n', type=int, default='5',help='Number of results that will be returned')
 @click.option('--radius', '-r', type=int, default='2',help='Radial distance away you want to search')
+@click.option('--json/--no-json', default=False, help='Flag allows for output to display as json')
+@click.option('--sort', default='rating', type=click.Choice(['rating', 'best', 'distance']), help='Specify how you wish to sort results')
+@click.option('--price', '-p', default='0', multiple=True, type=click.Choice(['0', '1', '2', '3', '4']), help='Specify price point of restaurants')
 @click.argument('addr')
-def find(addr, kind, number, radius):
+def find(addr, kind, number, radius, json, sort, price):
     """Search for the best food nearby"""
 
     location = get_location(addr)
@@ -25,10 +28,11 @@ def find(addr, kind, number, radius):
         coordinates = {'latitude': str(location.latitude), 
                         'longitude': str(location.longitude)}
         
-    response = search_yelp(coordinates, kind, str(number), miles_to_meters(radius))
+    response = search_yelp(coordinates, kind, str(number), miles_to_meters(radius), json, sort, price)
     #click.echo('latitude: ' + coordinates['latitude'] + ' longitude: ' + coordinates['longitude'])
     #click.echo('categories: ' + ','.join(kind))
     click.echo(response)
+    
 
 if __name__ == "__main__":
     main()
--- fp/funcmodule.py
+++ fp/funcmodule.py
@@ -1,6 +1,6 @@
 from geopy.geocoders import Nominatim
 import requests
-import simplejson as json
+import simplejson as simplejson
 import configparser
 
 def meters_to_miles(meters):
@@ -20,42 +20,54 @@ def get_location(addr):
     location = geolocator.geocode(addr)
     return location
 
-def search_yelp(coordinates, kinds, number, radius):
+def handle_error(radius, number):
+    if radius > 40000:
+        print('Radius too large. Please limit to <25.')
+        exit(1)
+    elif number > 50:
+        print('Cannot return that many results. Please limit number to <50.')
+        exit(1)
+
+def search_yelp(coordinates, kinds, number, radius, json, sort, price):
     """
     Given coordinates as a tuple and type, query yelp and return a json object
     """
+
+    # handle simple errors
+    handle_error(int(radius), int(number))
 
     # get our API secrets
     parser = configparser.ConfigParser()
     parser.read('CONFIG.cfg')
     key = parser.get('api-access', 'key')
-    client_id = parser.get('api-access', 'client_id')
+
+    headers = {'Authorization': 'Bearer ' + key,
+        'Content-Type': 'application/json'}
 
     
-
+    # build out the url with all search options
     url = 'https://api.yelp.com/v3/businesses/search?'
     url += '&' + 'term=food'
     url += '&' + 'latitude=' + coordinates['latitude']
     url += '&' + 'longitude=' + coordinates['longitude']
     url += '&' + 'limit=' + number
     url += '&' + 'radius=' + str(radius)
-    url += '&' + 'sort_by' + 'rating'
+    url += '&' + 'sort_by' + sort
     # tuple -> string separated by commas
     if kinds != ():
-        kind_string = ','.join(kinds)
-        url += '&' + 'categories=' + kind_string
+        url += '&' + 'categories=' + ','.join(kinds)
+    if price[0] != '0':
+        url += '&' + 'price=' + ','.join(price)
 
-    #print(url)
-
-    headers = {'Authorization': 'Bearer ' + key,
-        'Content-Type': 'application/json'}
     r = requests.get(url, headers=headers)
-    dirty_output = json.loads(r.text)
+    dirty_output = simplejson.loads(r.text)
     #print(dirty_output)
-    sanitized_output = json.dumps(sanitize_data(dirty_output), sort_keys=True, indent=4 * ' ')
-    pretty_string = pretty_print(sanitized_output)
-
-    return pretty_string
+    sanitized_output = simplejson.dumps(sanitize_data(dirty_output), indent=4 * ' ')
+    if json:
+        return sanitized_output
+    else:
+        pretty_string = pretty_print(sanitized_output)
+        return pretty_string
 
 def sanitize_data(raw_data):
     """
@@ -68,7 +80,7 @@ def sanitize_data(raw_data):
         info[business['name']] = {}
 
         # check each atrribute listed for each business and make sure we care about it, then add it to our info obj
-        desired_attrs = ['rating', 'is_closed', 'price', 'distance', 'phone', 'categories']
+        desired_attrs = ['rating', 'is_closed', 'price', 'distance', 'phone', 'categories', 'location']
         for item in desired_attrs:
             if item in business:
                 if item == 'distance':
@@ -80,6 +92,8 @@ def sanitize_data(raw_data):
                     info[business['name']][item] = ', '.join(category_list)
                 elif item == 'is_closed':
                     info[business['name']]['open'] = (business['is_closed'] == False)
+                elif item == 'location':
+                    info[business['name']]['location'] = business['location']['address1']
                 else:
                     info[business['name']][item] = business[item]
         
@@ -87,7 +101,7 @@ def sanitize_data(raw_data):
 
 def pretty_print(sanitized_data):
     output_string = ''
-    printer = json.loads(sanitized_data)
+    printer = simplejson.loads(sanitized_data)
     for name in printer:
         output_string += name + '\n'
         for info_item in printer[name]:
